2c8e069d6ef2f0970fb8bb98edc8c3795b527bc3
Corey Sterling <coreytsterling@gmail.com>
2022-01-20 14:26:55 +1300

    Implemented NominalAttributeMultiwayTest.

--- src/maia/ml/learner/standard/hoeffdingtree/split/test/NominalAttributeMultiwayTest.kt
+++ src/maia/ml/learner/standard/hoeffdingtree/split/test/NominalAttributeMultiwayTest.kt
@@ -1,8 +1,13 @@
 package maia.ml.learner.standard.hoeffdingtree.split.test
 
 import maia.ml.dataset.DataRow
+import maia.ml.dataset.type.standard.Nominal
+import maia.ml.dataset.util.isMissing
 import maia.ml.learner.standard.hoeffdingtree.HoeffdingTree
+import maia.util.Absent
 import maia.util.Optional
+import maia.util.Present
+import maia.util.assertType
 
 /**
  * TODO: What class does.
@@ -14,20 +19,24 @@ class NominalAttributeMultiwayTest(
 ) : SplitTest() {
 
     override fun branchForRow(row : DataRow) : Optional<Int> {
-        TODO("Not implemented yet.")
+        val type = assertType<Nominal<*, *, *, *>>(row.headers[attributeIndex].type)
+        return if (row.isMissing(type))
+            Absent
+        else
+            Present(row.getValue(type.indexRepresentation))
     }
 
-    override val maxBranches : Optional<Int>
-        get() = TODO("Not yet implemented")
+    override val maxBranches : Optional<Int> = Absent
 
-    override val attributesTestDependsOn : IntArray
-        get() = TODO("Not yet implemented")
+    override val attributesTestDependsOn : IntArray = IntArray(1) { attributeIndex }
 
     override fun describeConditionForBranch(
         branch : Int,
         tree : HoeffdingTree
     ) : String {
-        TODO("Not yet implemented")
+        val header = tree.trainHeaders[attributeIndex]
+        val type = assertType<Nominal<*, *, *, *>>(header.type)
+        return "${header.name} = ${type[branch]}"
     }
 
 }
