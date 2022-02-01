fd0f43612ff2c709610b0086d051c2675f2827ed
samst <samsteed4@gmail.com>
2022-02-02 11:25:37 +1300

    Basic version of Adaptive Random Forest working

--- src/maia/ml/learner/standard/hoeffdingtree/AdaptiveRandomForest.kt
+++ src/maia/ml/learner/standard/hoeffdingtree/AdaptiveRandomForest.kt
@@ -1,117 +1,117 @@
-//package maia.ml.learner.standard.hoeffdingtree
-//
-//import moa.classifiers.trees.RandomHoeffdingTree
-//import maia.ml.learner.Learner
-//import maia.ml.dataset.DataRow
-//import java.util.concurrent.ExecutorService
-//import java.util.concurrent.Executors
-//
-//import maia.ml.learner.type.LearnerType
-//
-//open class AdaptiveRandomForest : Ozabag(
-//    targetIndex = 0,
-//    ensembleSize = 10,
-//
-//)
-//
-//{
-//    public val FeaturesPerTree = 6
-//    public val lambdaOption: Float = 6.0F
-//    public  val numberOfJobs: Int = 1
-//    protected val FEATURES_M = 0
-//    protected val FEATURES_SQRT = 1
-//    protected val FEATURES_SQRT_INV = 2
-//    protected val FEATURES_PERCENT = 3
-//
-//
-//    protected val SINGLE_THREAD = 0
-//
-//    lateinit var ensemble2: RandomHoeffdingTree
-//    protected var instancesSeen: Long = 0
-//    protected var subspaceSize = 0
-//    private var executor: ExecutorService? = null
-//
-//
-//    open fun resetLearningImpl() {
-//        // Reset attributes
-//        ensemble = ArrayList<RandomHoeffdingTree>(emptyList())//emptyArray()
-//        subspaceSize = 0
-//        instancesSeen = 0
-//
-//        // Multi-threading
-//        val numberOfJobs: Int
-//        numberOfJobs =
-//            if (this.numberOfJobs === -1) Runtime.getRuntime()
-//                .availableProcessors() else this.numberOfJobs
-//        // SINGLE_THREAD and requesting for only 1 thread are equivalent.
-//        // this.executor will be null and not used...
-//        if (numberOfJobs != this.SINGLE_THREAD && numberOfJobs != 1) this.executor =
-//            Executors.newFixedThreadPool(numberOfJobs)
-//    }
-//
-//    protected override fun trainOnRow(
-//        row : DataRow
-//    ) {
-//        ++this.instancesSeen
-//        if(this.ensemble == null)
-//            initEnsemble(row);
-//
-//    }
-//
-//    protected open fun initEnsemble(row : DataRow) {
-//        // Init the ensemble.
-//        val ensembleSize: Int = this.ensembleSize
-//        ensemble2 = RandomHoeffdingTree()
-//
-//
-//        subspaceSize = this.FeaturesPerTree
-//
-//        // The size of m depends on:
-//        // 1) mFeaturesPerTreeSizeOption
-//        // 2) mFeaturesModeOption
+package maia.ml.learner.standard.hoeffdingtree
+
+import maia.ml.learner.standard.hoeffdingtree.RANDOM_HOEFFDING_TREE_LEARNER_TYPE
+import maia.ml.learner.Learner
+import maia.ml.dataset.DataRow
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+import maia.ml.learner.type.LearnerType
+
+open class AdaptiveRandomForest(targetIndex: Int) : Ozabag(
+    targetIndex = targetIndex,
+    ensembleSize = 10,
+)
+
+{
+    public val FeaturesPerTree = 6
+    public val lambdaOption: Float = 6.0F
+    public  val numberOfJobs: Int = 1
+    protected val FEATURES_M = 0
+    protected val FEATURES_SQRT = 1
+    protected val FEATURES_SQRT_INV = 2
+    protected val FEATURES_PERCENT = 3
+
+
+    protected val SINGLE_THREAD = 0
+
+    lateinit var ensemble2: RandomHoeffdingTree
+    protected var instancesSeen: Long = 0
+    protected var subspaceSize = 0
+    private var executor: ExecutorService? = null
+
+
+    open fun resetLearningImpl() {
+        // Reset attributes
+        ensemble = ArrayList<RandomHoeffdingTree>(emptyList())//emptyArray()
+        subspaceSize = 0
+        instancesSeen = 0
+
+        // Multi-threading
+        val numberOfJobs: Int
+        numberOfJobs =
+            if (this.numberOfJobs === -1) Runtime.getRuntime()
+                .availableProcessors() else this.numberOfJobs
+        // SINGLE_THREAD and requesting for only 1 thread are equivalent.
+        // this.executor will be null and not used...
+        if (numberOfJobs != this.SINGLE_THREAD && numberOfJobs != 1) this.executor =
+            Executors.newFixedThreadPool(numberOfJobs)
+    }
+
+    protected override fun trainOnRow(
+        row : DataRow
+    ) {
+        ++this.instancesSeen
+        if(this.ensemble == null)
+            initEnsemble(row);
+
+    }
+
+    protected open fun initEnsemble(row : DataRow) {
+        // Init the ensemble.
+        val ensembleSize: Int = this.ensembleSize
+        ensemble2 = RandomHoeffdingTree(targetIndex)
+
+
+        subspaceSize = this.FeaturesPerTree
+
+        // The size of m depends on:
+        // 1) mFeaturesPerTreeSizeOption
+        // 2) mFeaturesModeOption
 //        val n: Int = row.numColumns - 1
-//        //when (this.mFeaturesModeOption.getChosenIndex()) {
-//        //    this.FEATURES_SQRT -> subspaceSize = Math.round(Math.sqrt(n.toDouble()))
+//        when (this.mFeaturesModeOption.getChosenIndex()) {
+//            this.FEATURES_SQRT -> subspaceSize = Math.round(Math.sqrt(n.toDouble()))
 //                .toInt() + 1
-//        //    this.FEATURES_SQRT_INV -> subspaceSize =
-//        //        n - Math.round(Math.sqrt(n.toDouble()) + 1)
+//            this.FEATURES_SQRT_INV -> subspaceSize =
+//                n - Math.round(Math.sqrt(n.toDouble()) + 1)
 //                    .toInt()
-//        //    this.FEATURES_PERCENT -> {
+//            this.FEATURES_PERCENT -> {
 //                // If subspaceSize is negative, then first find out the actual percent, i.e., 100% - m.
-//        //        val percent =
-//        //            if (subspaceSize < 0) (100 + subspaceSize) / 100.0 else subspaceSize / 100.0
-//        //        subspaceSize = Math.round(n * percent).toInt()
-//        //    }
-//        //}
-//        // Notice that if the selected mFeaturesModeOption was
-//        //  AdaptiveRandomForest.FEATURES_M then nothing is performed in the
-//        //  previous switch-case, still it is necessary to check (and adjusted)
-//        //  for when a negative value was used.
-//
-////        // m is negative, use size(features) + -m
-////        if (subspaceSize < 0) subspaceSize = n + subspaceSize
-////        // Other sanity checks to avoid runtime errors.
-////        //  m <= 0 (m can be negative if this.subspace was negative and
-////        //  abs(m) > n), then use m = 1
-////        if (subspaceSize <= 0) subspaceSize = 1
-////        // m > n, then it should use n
-////        if (subspaceSize > n) subspaceSize = n
-////        val treeLearner: RandomHoeffdingTree =
-////            getPreparedClassOption(this.treeLearnerOption) as ARFHoeffdingTree
-////        treeLearner.resetLearning()
-////        for (i in 0 until ensembleSize) {
-////            treeLearner.subspaceSizeOption.setValue(subspaceSize)
-////            ensemble[i] = ARFBaseLearner(
-////                i,
-////                treeLearner.copy() as ARFHoeffdingTree,
-////                classificationEvaluator.copy() as BasicClassificationPerformanceEvaluator,
-////                instancesSeen,
-////                !this.disableBackgroundLearnerOption.isSet(),
-////                !this.disableDriftDetectionOption.isSet(),
-////                driftDetectionMethodOption,
-////                warningDetectionMethodOption,
-////                false
-////            )
-////        }
-//    }
-//}
+//                val percent =
+//                    if (subspaceSize < 0) (100 + subspaceSize) / 100.0 else subspaceSize / 100.0
+//                subspaceSize = Math.round(n * percent).toInt()
+//            }
+//        }
+
+        // Notice that if the selected mFeaturesModeOption was
+        //  AdaptiveRandomForest.FEATURES_M then nothing is performed in the
+        //  previous switch-case, still it is necessary to check (and adjusted)
+        //  for when a negative value was used.
+
+//        // m is negative, use size(features) + -m
+//        if (subspaceSize < 0) subspaceSize = n + subspaceSize
+//        // Other sanity checks to avoid runtime errors.
+//        //  m <= 0 (m can be negative if this.subspace was negative and
+//        //  abs(m) > n), then use m = 1
+//        if (subspaceSize <= 0) subspaceSize = 1
+//        // m > n, then it should use n
+//        if (subspaceSize > n) subspaceSize = n
+//        val treeLearner: RandomHoeffdingTree =
+//            getPreparedClassOption(this.treeLearnerOption) as ARFHoeffdingTree
+//        treeLearner.resetLearning()
+//        for (i in 0 until ensembleSize) {
+//            treeLearner.subspaceSizeOption.setValue(subspaceSize)
+//            ensemble[i] = ARFBaseLearner(
+//                i,
+//                treeLearner.copy() as ARFHoeffdingTree,
+//                classificationEvaluator.copy() as BasicClassificationPerformanceEvaluator,
+//                instancesSeen,
+//                !this.disableBackgroundLearnerOption.isSet(),
+//                !this.disableDriftDetectionOption.isSet(),
+//                driftDetectionMethodOption,
+//                warningDetectionMethodOption,
+//                false
+//            )
+//        }
+    }
+}
--- src/maia/ml/learner/standard/hoeffdingtree/RandomHoeffdingTree.kt
+++ src/maia/ml/learner/standard/hoeffdingtree/RandomHoeffdingTree.kt
@@ -1,68 +1,66 @@
-///*
-// *    RandomHoeffdingTree.java
-// *    Copyright (C) 2010 University of Waikato, Hamilton, New Zealand
-// *    @author Albert Bifet (abifet@cs.waikato.ac.nz)
-// *
-// *    This program is free software; you can redistribute it and/or modify
-// *    it under the terms of the GNU General Public License as published by
-// *    the Free Software Foundation; either version 3 of the License, or
-// *    (at your option) any later version.
-// *
-// *    This program is distributed in the hope that it will be useful,
-// *    but WITHOUT ANY WARRANTY; without even the implied warranty of
-// *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// *    GNU General Public License for more details.
-// *
-// *    You should have received a copy of the GNU General Public License
-// *    along with this program. If not, see <http://www.gnu.org/licenses/>.
-// *
-// */
-//package moa.classifiers.trees
-//
-//import maia.ml.dataset.headers.DataColumnHeaders
-//import maia.ml.dataset.headers.DataColumnHeadersView
-//import maia.ml.dataset.type.standard.Nominal
-//import maia.ml.dataset.util.allColumnsExcept
-//import maia.ml.learner.standard.hoeffdingtree.HOEFFDING_TREE_LEARNER_TYPE
-//import maia.ml.learner.standard.hoeffdingtree.HoeffdingTree
-//import maia.ml.learner.standard.hoeffdingtree.LeafPredictor
-//import maia.ml.learner.standard.hoeffdingtree.node.LearningNode
-//import maia.ml.learner.standard.hoeffdingtree.node.LearningNodeNB
-//import maia.ml.learner.standard.hoeffdingtree.node.LearningNodeNBAdaptive
-//import maia.ml.learner.standard.hoeffdingtree.node.RandomLearningNode
-//import maia.ml.learner.standard.hoeffdingtree.util.ObservedClassDistribution
-//import maia.ml.learner.type.*
-//import maia.util.asIterable
-//import maia.util.datastructure.OrderedHashSet
-//import maia.util.enumerate
-//
-//
-///**
-// * Random decision trees for data streams.
-// *
-// * @author Albert Bifet (abifet at cs dot waikato dot ac dot nz)
-// * @version $Revision: 7 $
-// */
-//open class RandomHoeffdingTree : HoeffdingTree() {
-//
-//    fun RandomHoeffdingTree() {
-//        this.removePoorAttributes = false
-//
-//    }
-//
-//    fun isRandomizable(): Boolean {
-//        return true
-//    }
-//
-//    override fun newLearningNode(
-//        initialClassObservations: ObservedClassDistribution?
-//    ): LearningNode {
-//        val observations = initialClassObservations ?: ObservedClassDistribution(classType.numCategories)
-//
-//        return when (leafPredictor) {
-//            LeafPredictor.MAJORITY_CLASS -> RandomLearningNode(this, observations, true)
-//            LeafPredictor.NAIVE_BAYES -> LearningNodeNB(this, observations, true)
-//            LeafPredictor.NAIVE_BAYES_ADAPTIVE -> LearningNodeNBAdaptive(this, observations, true)
-//        }
-//    }
-//}
+/*
+ *    RandomHoeffdingTree.java
+ *    Copyright (C) 2010 University of Waikato, Hamilton, New Zealand
+ *    @author Albert Bifet (abifet@cs.waikato.ac.nz)
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package maia.ml.learner.standard.hoeffdingtree
+
+import maia.ml.dataset.headers.DataColumnHeaders
+import maia.ml.dataset.headers.DataColumnHeadersView
+import maia.ml.dataset.type.standard.Nominal
+import maia.ml.dataset.util.allColumnsExcept
+import maia.ml.learner.standard.hoeffdingtree.HOEFFDING_TREE_LEARNER_TYPE
+import maia.ml.learner.standard.hoeffdingtree.HoeffdingTree
+import maia.ml.learner.standard.hoeffdingtree.LeafPredictor
+import maia.ml.learner.standard.hoeffdingtree.node.LearningNode
+import maia.ml.learner.standard.hoeffdingtree.node.LearningNodeNB
+import maia.ml.learner.standard.hoeffdingtree.node.LearningNodeNBAdaptive
+import maia.ml.learner.standard.hoeffdingtree.node.RandomLearningNode
+import maia.ml.learner.standard.hoeffdingtree.util.ObservedClassDistribution
+import maia.ml.learner.type.*
+import maia.util.asIterable
+import maia.util.datastructure.OrderedHashSet
+import maia.util.enumerate
+
+
+/**
+ * Random decision trees for data streams.
+ *
+ * @author Albert Bifet (abifet at cs dot waikato dot ac dot nz)
+ * @version $Revision: 7 $
+ */
+open class RandomHoeffdingTree(targetIndex: Int) : HoeffdingTree(targetIndex) {
+    fun RandomHoeffdingTree() {
+        this.removePoorAttributes = false
+    }
+
+    fun isRandomizable(): Boolean {
+        return true
+    }
+
+    override fun newLearningNode(
+        initialClassObservations: ObservedClassDistribution?
+    ): LearningNode {
+        val observations = initialClassObservations ?: ObservedClassDistribution(classType.numCategories)
+
+        return when (leafPredictor) {
+            LeafPredictor.MAJORITY_CLASS -> RandomLearningNode(this, observations, true)
+            LeafPredictor.NAIVE_BAYES -> LearningNodeNB(this, observations, true)
+            LeafPredictor.NAIVE_BAYES_ADAPTIVE -> LearningNodeNBAdaptive(this, observations, true)
+        }
+    }
+}
